818c3d7624e59e4e31c6dbd3b44bc2e961347115
Juan_Carlos <juancarlospaco@gmail.com>
2022-12-13 19:25:20 -0300

    init

--- index.js
+++ index.js
@@ -92,18 +92,18 @@ if (context.eventName === "issue_comment") {
       const cmd = parseGithubCommand(githubComment)
       // Add Reaction of "Eyes" as seen.
       if (addReaction(githubClient, "eyes")) {
-        const start = performance.now()
-        const output = executeShebangScript(cmd, codes)
+        const started  = performance.now()
+        const output   = executeShebangScript(cmd, codes)
         const finished = performance.now()
         console.log("OK_OUTPUT\t", output)
         if (addReaction(githubClient, (output.length > 0 ? "+1" : "-1"))) {
           console.warn("HERE");
           const comment = `
           <details>
-          <summary>Bench</summary>
-          started\t${start}
-          finished\t${finished}
-          duration\t${finished - start}
+            <summary>Bench</summary>
+            <b>started </b>  <code>${started}</code><br>
+            <b>finished</b>  <code>${finished}</code><br>
+            <b>duration</b>  <code>${finished - started}</code><br>
           </details>
           <code>${output}</code>
           `
