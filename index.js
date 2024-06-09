7a35b5fb8f7a230063e7522bba67309e10167185
Juan Carlos <juancarlospaco@gmail.com>
2024-06-09 20:39:02 -0300

    Update index.js
    0 bytes is just "0"

--- index.js
+++ index.js
@@ -63,9 +63,8 @@ function formatSizeUnits(bytes) {
   if      (bytes >= 1073741824) { bytes = (bytes / 1073741824).toFixed(2) + " Gb"; }
   else if (bytes >= 1048576)    { bytes = (bytes / 1048576).toFixed(2) + " Mb"; }
   else if (bytes >= 1024)       { bytes = (bytes / 1024).toFixed(2) + " Kb"; }
-  else if (bytes >  1)          { bytes = bytes + " bytes"; }
-  else if (bytes == 1)          { bytes = bytes + " byte"; }
-  else                          { bytes = "0 bytes"; }
+  else if (bytes >=  1)         { bytes = bytes + " bytes"; }
+  else                          { bytes = "0"; }
   return bytes + bites;
 }
 
